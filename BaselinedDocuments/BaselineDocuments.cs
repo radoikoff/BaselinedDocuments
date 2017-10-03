cf4fe21795166f3b3def11fe2e2cc38c5f5c45f2
radoikoff <radoikoff@gmail.com>
2017-10-03 15:34:15 +0300

    Added check if mapping data is valid. Simplified error checking. Simplified OutputWriter

--- BaselinedDocuments/BaselineDocuments.cs
+++ BaselinedDocuments/BaselineDocuments.cs
@@ -14,58 +14,30 @@ namespace BaselinedDocuments
         static void Main()
         {
             var statements = new List<Statement>();
-
-            if (IsRequiredPathsValid())
-            {
-                GetMappingData(statements);
-                CreateUpdateArchives(statements);
-            }
-        }
-
-        private static void GetMappingData(List<Statement> statements)
-        {
-            string mappingFile = ConfigurationManager.AppSettings.Get("MappingFile");
-
-            if (!File.Exists(mappingFile))
-            {
-                OutputWriter.DisplayMessage("Mapping file does not exist");
-                OutputWriter.WriteMessageInLogFile("Mapping file does not exist");
-                return;
-            }
-
-            string[] mappingData = File.ReadAllLines(mappingFile);
-
-            foreach (var line in mappingData)
-            {
-                string[] tokens = line.Split(',');   //to check if data is valid
-                string statName = tokens[0].Trim();
-                string docTitle = tokens[2].Trim();
-                string docFileName = tokens[3].Trim();
-
-                if (!statements.Any(s => s.Name.Equals(statName)))
-                {
-                    var stat = new Statement();
-                    stat.Name = statName;
-                    statements.Add(stat);
-                }
-
-                var document = new Document(docTitle, docFileName);
-
-                statements.FirstOrDefault(s => s.Name.Equals(statName)).Documents.Add(document);
-            }
-        }
-
-        private static void CreateUpdateArchives(List<Statement> statements)
-        {
             string sourcePathMaster = ConfigurationManager.AppSettings.Get("SourcePathMaster");
             string sourcePathDraft = ConfigurationManager.AppSettings.Get("SourcePathDraft");
-            string destinationPath = ConfigurationManager.AppSettings.Get("DestinationPath");
-            string newZipFilesPath = ConfigurationManager.AppSettings.Get("NewZipFilesPath");
-
             var sourceDirInfoMaster = new DirectoryInfo(sourcePathMaster);
             var sourceDirInfoDraft = new DirectoryInfo(sourcePathDraft);
             string[] sourceMasterFileNames = sourceDirInfoMaster.GetFiles().Select(f => f.Name).ToArray();
             string[] sourceDraftFileNames = sourceDirInfoDraft.GetFiles().Select(f => f.Name).ToArray();
+
+            if (!IsRequiredPathsValid())
+            {
+                OutputWriter.DisplayMessageAndAddToLogFile("ERROR: One or more of defined folder paths are invalid");
+                return;
+            }
+
+            GetMappingData(statements);
+
+            List<string> missingFiles = GetMissingFiles(statements, sourceMasterFileNames, sourceDraftFileNames);
+            if (missingFiles.Any())
+            {
+                OutputWriter.DisplayMessageAndAddToLogFile("ERROR: The following files are missing:\r\n" + string.Join("\r\n", missingFiles));
+                return;
+            }
+
+            string destinationPath = ConfigurationManager.AppSettings.Get("DestinationPath");
+            string newZipFilesPath = ConfigurationManager.AppSettings.Get("NewZipFilesPath");
 
             foreach (var statement in statements)
             {
@@ -77,26 +49,64 @@ namespace BaselinedDocuments
                 var destinationDirInfo = new DirectoryInfo(destinationPath);
                 var existingZipFileNames = destinationDirInfo.GetFiles("*.zip").Select(f => Path.GetFileNameWithoutExtension(f.Name));
 
-                if (IsDocumentsAvailable(statement, sourceMasterFileNames, sourceDraftFileNames))
+                if (existingZipFileNames.Contains(archiveName)) //Update
                 {
-                    if (existingZipFileNames.Contains(archiveName)) //Update
-                    {
-                        fullArchiveName = Path.Combine(destinationPath, archiveName) + ".zip";
-                        msgTextSuccess = archiveName + " updated successfully";
-                        CreateUpdateZip(statement, sourcePathMaster, sourcePathDraft, destinationPath, sourceMasterFileNames, sourceDraftFileNames, fullArchiveName, msgTextSuccess);
-                    }
-                    else //create
-                    {
-                        fullArchiveName = Path.Combine(destinationPath, newZipFilesPath, archiveName) + ".zip";
-                        msgTextSuccess = archiveName + " created successfuly";
-                        CreateUpdateZip(statement, sourcePathMaster, sourcePathDraft, destinationPath, sourceMasterFileNames, sourceDraftFileNames, fullArchiveName, msgTextSuccess);
-                    }
+                    fullArchiveName = Path.Combine(destinationPath, archiveName) + ".zip";
+                    msgTextSuccess = archiveName + " updated successfully";
+                    CreateUpdateZip(statement, sourcePathMaster, sourcePathDraft, destinationPath, sourceMasterFileNames, sourceDraftFileNames, fullArchiveName, msgTextSuccess);
                 }
-                else
+                else //create
                 {
-                    OutputWriter.DisplayMessage($"ERROR: Not all required files are avaliable for statetment {statement.Name}");
-                    OutputWriter.WriteMessageInLogFile($"ERROR: Not all required files are avaliable for statetment {statement.Name}");
+                    fullArchiveName = Path.Combine(destinationPath, newZipFilesPath, archiveName) + ".zip";
+                    msgTextSuccess = archiveName + " created successfuly";
+                    CreateUpdateZip(statement, sourcePathMaster, sourcePathDraft, destinationPath, sourceMasterFileNames, sourceDraftFileNames, fullArchiveName, msgTextSuccess);
                 }
+            }
+        }
+
+        private static void GetMappingData(List<Statement> statements)
+        {
+            string mappingFile = ConfigurationManager.AppSettings.Get("MappingFile");
+
+            if (!File.Exists(mappingFile))
+            {
+                OutputWriter.DisplayMessageAndAddToLogFile("Mapping file does not exist at specified location");
+                return;
+            }
+
+            string[] mappingData = File.ReadAllLines(mappingFile);
+
+            string[] tokens;
+            string statName = string.Empty;
+            string docTitle = string.Empty;
+            string docFileName = string.Empty;
+
+            foreach (var line in mappingData)
+            {
+                try
+                {
+                    tokens = line.Split(',');
+                    statName = tokens[0].Trim();
+                    docTitle = tokens[2].Trim();
+                    docFileName = tokens[3].Trim();
+                }
+                catch
+                {
+                    OutputWriter.DisplayMessageAndAddToLogFile($"ERROR: Map line \"{line}\" is invalid");
+                    statements.Clear();
+                    return;
+                }
+
+                if (!statements.Any(s => s.Name.Equals(statName)))
+                {
+                    var stat = new Statement();
+                    stat.Name = statName;
+                    statements.Add(stat);
+                }
+
+                var document = new Document(docTitle, docFileName);
+
+                statements.FirstOrDefault(s => s.Name.Equals(statName)).Documents.Add(document);
             }
         }
 
@@ -127,27 +137,26 @@ namespace BaselinedDocuments
                             newDocumentName = newDocumentName + " " + filesTimeStamp + Path.GetExtension(document.FileName);
                             archive.CreateEntryFromFile(Path.Combine(sourcePath, document.FileName), Path.Combine(baselineFolderName, newDocumentName));
                         }
-                        OutputWriter.DisplayMessage(msgTextSuccess);
-                        OutputWriter.WriteMessageInLogFile(msgTextSuccess);
+                        OutputWriter.DisplayMessageAndAddToLogFile(msgTextSuccess);
                     }
                     else
                     {
-                        OutputWriter.DisplayMessage($"ERROR: Folder {baselineFolderName} already exisits in {fullArchiveName}");
-                        OutputWriter.WriteMessageInLogFile($"ERROR: Folder {baselineFolderName} already exisits in {fullArchiveName}");
+                        OutputWriter.DisplayMessageAndAddToLogFile($"ERROR: Folder {baselineFolderName} already exisits in {fullArchiveName}");
                     }
                 }
             }
             catch (Exception exception)
             {
-                OutputWriter.DisplayMessage($"ERROR: Unable to open or update {fullArchiveName} : {exception.Message}");
-                OutputWriter.WriteMessageInLogFile($"ERROR: Unable to open or update {fullArchiveName} : {exception.Message}");
+                OutputWriter.DisplayMessageAndAddToLogFile($"ERROR: Unable to open or update {fullArchiveName} : {exception.Message}");
             }
         }
 
-        private static bool IsDocumentsAvailable(Statement statement, string[] sourceMasterFileNames, string[] sourceDraftFileNames)
+        private static List<string> GetMissingFiles(List<Statement> statements, string[] sourceMasterFileNames, string[] sourceDraftFileNames)
         {
+            var requiredFileNames = statements.SelectMany(s => s.Documents.Select(d => d.FileName));
             var combinedFileNames = sourceMasterFileNames.Union(sourceDraftFileNames);
-            return combinedFileNames.Intersect(statement.Documents.Select(d => d.FileName)).Count() == statement.Documents.Count();
+            var notFoundFiles = requiredFileNames.Except(combinedFileNames).ToList();
+            return notFoundFiles;
         }
 
         private static bool IsRequiredPathsValid()
@@ -163,8 +172,7 @@ namespace BaselinedDocuments
             }
             else
             {
-                OutputWriter.DisplayMessage("Specified directory for master files does not exist");
-                OutputWriter.WriteMessageInLogFile("Specified directory for master files does not exist");
+                OutputWriter.DisplayMessageAndAddToLogFile("Specified directory for master files does not exist");
             }
 
             if (Directory.Exists(ConfigurationManager.AppSettings.Get("SourcePathDraft")))
@@ -173,8 +181,7 @@ namespace BaselinedDocuments
             }
             else
             {
-                OutputWriter.DisplayMessage("Specified directory for draft files does not exist");
-                OutputWriter.WriteMessageInLogFile("Specified directory for draft files does not exist");
+                OutputWriter.DisplayMessageAndAddToLogFile("Specified directory for draft files does not exist");
             }
 
             if (Directory.Exists(ConfigurationManager.AppSettings.Get("DestinationPath")))
@@ -183,8 +190,7 @@ namespace BaselinedDocuments
             }
             else
             {
-                OutputWriter.DisplayMessage("Specified destination directory does not exist");
-                OutputWriter.WriteMessageInLogFile("Specified destination directory does not exist");
+                OutputWriter.DisplayMessageAndAddToLogFile("Specified destination directory does not exist");
             }
 
             if (Directory.Exists(ConfigurationManager.AppSettings.Get("NewZipFilesPath")))
@@ -193,8 +199,7 @@ namespace BaselinedDocuments
             }
             else
             {
-                OutputWriter.DisplayMessage("Specified directory for newly created archives does not exist");
-                OutputWriter.WriteMessageInLogFile("Specified directory for newly created archives does not exist");
+                OutputWriter.DisplayMessageAndAddToLogFile("Specified directory for newly created archives does not exist");
             }
 
             if (masterPathExists && draftPathExists && destinationPathExists && NewZipFilesPathExists)
--- BaselinedDocuments/OutputWriter.cs
+++ BaselinedDocuments/OutputWriter.cs
@@ -15,16 +15,15 @@ namespace BaselinedDocuments
             Console.WriteLine(message);
         }
 
-        //public static void DisplayException(string message)
-        //{
-        //    ConsoleColor currentColor = Console.ForegroundColor;
-        //    Console.ForegroundColor = ConsoleColor.Red;
-        //    Console.WriteLine(message);
-        //    Console.ForegroundColor = currentColor;
-        //}
-
         public static void WriteMessageInLogFile(string message)
         {
+            string logFileName = ConfigurationManager.AppSettings.Get("LogFileName");
+            File.AppendAllText(logFileName, DateTime.Now + " : " + message + "\r\n");
+        }
+
+        public static void DisplayMessageAndAddToLogFile(string message)
+        {
+            Console.WriteLine(message);
             string logFileName = ConfigurationManager.AppSettings.Get("LogFileName");
             File.AppendAllText(logFileName, DateTime.Now + " : " + message + "\r\n");
         }
